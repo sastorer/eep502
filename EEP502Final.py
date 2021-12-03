e1cf5b4a007b369c9b68cc2e097f39713c8a5c37
Sarah Storer <sstorer3@gatech.edu>
2021-12-02 16:42:53 -0800

    2/3 parts done

--- EEP502Final.py
+++ EEP502Final.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 import re
 
+import nltk
+nltk.download('words')
+
+from nltk.corpus import words
+
 def transition_matrix(transitions):
     df = pd.DataFrame(transitions)
 
@@ -27,25 +32,38 @@ f = open(strPath)
 strText = f.read()
 strText = re.sub('([.,!?():;])', r' \1 ', strText)
 strText = re.sub('\s{2,}', ' ', strText)
-listText = strText.split()
-m = transition_matrix(listText)
-uniqueText = np.unique(listText)
 
-pairs = make_pairs(listText)
+listText = strText.split()
+pairsWords = make_pairs(listText)
+wordTransition = transition_matrix(listText)
+uniqueText = np.unique(listText)
 wordDict = {}
-for word_1, word_2 in pairs:
+for word_1, word_2 in pairsWords:
     if word_1 in wordDict.keys():
         wordDict[word_1].append(word_2)
     else:
         wordDict[word_1] = [word_2]
 
+letterText = []
+for letter in strText:
+    if letter != '\n':
+        letterText.append(letter)
+letterTransition = transition_matrix(letterText)
+uniqueLetters = np.unique(letterText)
+pairsLetters = make_pairs(letterText)
+letterDict = {}
+for letter_1, letter_2 in pairsLetters:
+    if letter_1 in letterDict.keys():
+        letterDict[letter_1].append(letter_2)
+    else:
+        letterDict[letter_1] = [letter_2]
 
 def generate_consitution_words(n):
     wordList = []
     first_word = "word"
     while first_word.islower():
         first_word = np.random.choice(listText)
-        if first_word in ['!', ',', '.', '?', ':', ';']:
+        if first_word in ['!', ',', '.', '?', ':', ';', '-', '(', ')']:
             first_word = np.random.choice(listText)
     wordList.append(first_word)
 
@@ -53,9 +71,41 @@ def generate_consitution_words(n):
         probabilities = []
         uniqueDict = np.unique(wordDict[wordList[-1]])
         for entry in uniqueDict:
-            probabilities.append(m[np.where(uniqueText == wordList[-1])[0][0]][np.where(uniqueText == entry)[0][0]])
+            probabilities.append(wordTransition[np.where(uniqueText == wordList[-1])[0][0]][np.where(uniqueText == entry)[0][0]])
         wordList.append(np.random.choice(uniqueDict, p=probabilities))
 
     print(' '.join(wordList) + '.')
+    return ' '.join(wordList) + '.'
 
+def generate_constitution_letters(n):
+    letterList = []
+    first_letter = "w"
+    while first_letter.islower():
+        first_letter = np.random.choice(letterText)
+        if first_letter in ['!', ',', '.', '?', ':', ';', '-', '(', ')', ' ']:
+            first_letter = np.random.choice(letterText)
+    letterList.append(first_letter)
+
+    count = n
+    while count != 0:
+        probabilities = []
+        uniqueLetterDict = np.unique(letterDict[letterList[-1]])
+        for entry in uniqueLetterDict:
+            probabilities.append(letterTransition[np.where(uniqueLetters == letterList[-1])[0][0]][np.where(uniqueLetters == entry)[0][0]])
+        letterList.append(np.random.choice(uniqueLetterDict, p=probabilities))
+        if letterList[-1] == ' ':
+            count = count - 1
+
+    print(''.join(letterList))
+    return ''.join(letterList)
+
+letterstr = generate_constitution_letters(100)
+setofwords = set(words.words())
+wordCount = 0
+for word in letterstr.split():    
+    if word in setofwords:
+        if len(word) != 1:
+            wordCount = wordCount + 1
+print("There are {} words here.".format(wordCount))
+print('\n')
 generate_consitution_words(500)
